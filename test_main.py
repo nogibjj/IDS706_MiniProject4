0743fe7e8d7f37226c1d8a9db1112878e34aee09
ll442 <ll442@duke.edu>
2023-09-10 22:13:33 -0400

    ci update1

---
+++ test_main.py
@@ -0,0 +1,12 @@
+from main import add
+
+def test_func(a,b):
+    result = add(a,b)
+    template = a+b
+    assert result==template
+
+if __name__ == "__main__":
+    test_func(1,2)
+    test_func(3,4)
+    test_func(-2,-3)
+    print("All test passed")
