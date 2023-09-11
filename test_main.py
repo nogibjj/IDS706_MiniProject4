454438b527243ab58fcef09821c72752f922802d
ll442 <ll442@duke.edu>
2023-09-10 22:32:25 -0400

    ci update4

--- test_main.py
+++ test_main.py
@@ -1,14 +1,24 @@
 from main import add
 
 
-def test_func(a, b):
-    result = add(a, b)
-    template = a+b
+def test_func_positive():
+    result = add(3, 5)
+    template = 8
+    assert result == template
+
+def test_func_negative():
+    result = add(-2, -3)
+    template = -5
+    assert result == template
+
+def test_func_pos_neg():
+    result = add(1, -3)
+    template = -2
     assert result == template
 
 
 if __name__ == "__main__":
-    test_func(1, 2)
-    test_func(3, 4)
-    test_func(-2, -3)
+    test_func_positive()
+    test_func_negative()
+    test_func_pos_neg()
     print("All test passed")
